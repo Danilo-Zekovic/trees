b940f2e4be65d8d6411740774a515e8ddbfe72ce
Danilo Zekovic <dzv9776@saintjoe.edu>
2015-02-27 22:05:50 -0500

    Update BSTtree.js
    Added new methods after insert

--- BSTtree.js
+++ BSTtree.js
@@ -44,3 +44,106 @@ function insert(data) {
  }
 }
 
+function remove(data) {
+	 root = removeNode(this.root, data);
+}
+function removeNode(node, data) {
+	 if (node === null) {
+		  return null;
+		   }
+	  if (data == node.data) {
+		  // node has no children
+		  if (node.left === null && node.right === null) {
+			   return null;
+			    }
+		  // node has no left child
+		  if (node.left === null) {
+			   return node.right;
+			    }
+		  // node has no right child
+		  if (node.right === null) {
+			   return node.left;
+			    }
+		  // node has two children
+		  var tempNode = getSmallest(node.right);
+		   node.data = tempNode.data;
+		    node.right = this.removeNode(node.right, tempNode.data);
+		     return node;
+		      }
+	   else if (data < node.data) {
+		    node.left = this.removeNode(node.left, data);
+		     return node;
+		      }
+	    else {
+		     node.right = this.removeNode(node.right, data);
+		      return node;
+		       }
+}
+
+// change value in data
+function update(data) {
+	 var grade = this.find(data);
+	  grade.count++;
+	   return grade;
+}
+
+// display array contents
+function prArray(arr) {
+	 putstr(arr[0].toString() + ' ');
+	  for (var i = 1; i < arr.length; ++i) {
+		   putstr(arr[i].toString() + ' ');
+		    if (i % 10 === 0) {
+			     putstr("\n");
+			      }
+		     }
+}
+
+// create an array with specified length and fill with random integers
+function genArray(length) {
+	 var arr = [];
+	  for (var i = 0; i < length; ++i) {
+		   arr[i] = Math.floor(Math.random() * 101);
+		    }
+	   return arr;
+}
+
+function getSmallest(node) {
+   if (node.left == null) {
+      return node;
+   }
+   else {
+      return getSmallest(node.left);
+   }
+}
+
+function getMin() {
+  var current = this.root;
+  while (!(current.left == null)) {
+    current = current.left;	
+  }
+  return current.data;
+}
+
+function getMax() {
+  var current = this.root;
+  while (!(current.right == null)) {
+    current = current.right;
+  }
+  return current.data;
+}
+
+function find(data) {
+  var current = this.root;
+  while (current.data != data) {
+    if (data < current.data) {
+      current = current.left;
+    }
+    else {
+      current = current.right;
+    }
+    if (current == null) {
+      return null;
+    }
+  }
+  return current;
+}
