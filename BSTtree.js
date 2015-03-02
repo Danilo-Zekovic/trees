2c92a52f59987dcb5c18226bf4bf55592936490c
Danilo Zekovic <dzv9776@saintjoe.edu>
2015-03-01 20:30:21 -0500

    Update BSTtree.js

--- BSTtree.js
+++ BSTtree.js
@@ -116,22 +116,6 @@ function getSmallest(node) {
    }
 }
 
-function getMin() {
-  var current = this.root;
-  while (!(current.left == null)) {
-    current = current.left;	
-  }
-  return current.data;
-}
-
-function getMax() {
-  var current = this.root;
-  while (!(current.right == null)) {
-    current = current.right;
-  }
-  return current.data;
-}
-
 function find(data) {
   var current = this.root;
   while (current.data != data) {
